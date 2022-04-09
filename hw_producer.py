c6e8a07210a6ee1a71909a755031ce6ce749c6cf
JessieCaroline <jessiecaroline.jc@gmail.com>
2022-04-09 14:15:22 -0400

    Update hw_producer.py

--- hw_producer.py
+++ hw_producer.py
@@ -4,7 +4,7 @@ import socket
 import requests
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 22255        # Port to listen on (non-privileged ports are > 1023)
+PORT = 22257        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
